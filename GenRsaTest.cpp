2bfecec7f84bbb5ac5d7b916627f4e76693357aa
Jan-Espen Oversand <sigsegv@radiotube.org>
2024-01-05 16:24:00 +0100

    Initial commit

---
+++ GenRsaTest.cpp
@@ -0,0 +1,40 @@
+//
+// Created by sigsegv on 12/26/23.
+//
+
+#include "include/JwkPemRsaKey.h"
+#include "include/Openssl.h"
+#include "include/OpensslRsa.h"
+#include <iostream>
+
+int main() {
+    JwkPemRsaKey rsa{};
+    Openssl::Seed(256 / 8);
+    OpensslRsa rawRsa = rsa.GenerateRandomInternal();
+    auto jwk = rsa.ToJwk();
+    std::cout << jwk << "\n";
+    auto pem = rsa.ToTraditionalPrivatePem();
+    std::cout << pem << "\n";
+    auto pem2 = rsa.ToTraditionalPrivatePem();
+    if (pem != pem2) {
+        std::cout << pem2 << "\n";
+        std::cerr << "Mismatch 1\n";
+        return 1;
+    }
+    rsa.FromJwk(jwk);
+    auto rawRsa2 = rsa.CreateOpensslRsa();
+    if (*rawRsa2 != rawRsa) {
+        std::cerr << "Key mismatch at low level\n";
+        return 1;
+    }
+    auto pem3 = rsa.ToTraditionalPrivatePem();
+    if (pem != pem3) {
+        std::cout << pem3 << "\n";
+        std::cerr << "Mismatch reimport\n";
+        return 1;
+    }
+    if (jwk != rsa.ToJwk()) {
+        std::cerr << "Mismatch jwk\n";
+        return 1;
+    }
+}
---
+++ include/OidcTokenRequest.h
@@ -0,0 +1,32 @@
+//
+// Created by sigsegv on 12/25/23.
+//
+
+#ifndef DRWHATSNOT_TOKENURL_H
+#define DRWHATSNOT_TOKENURL_H
+
+#include <string>
+#include <map>
+#include <vector>
+
+struct OidcPostRequest {
+    std::string url{};
+    std::map<std::string,std::string> params{};
+};
+
+class OidcTokenRequest {
+private:
+    std::string url;
+    std::string clientId;
+    std::string jwk;
+    std::string redirectUri;
+    std::string code;
+    std::vector<std::string> scope;
+    std::string codeVerifier;
+public:
+    OidcTokenRequest(const std::string &url, const std::string &clientId, const std::string &jwk, const std::string &redirectUri, const std::string &code, const std::vector<std::string> &scope, const std::string &codeVerifier) : url(url), clientId(clientId), jwk(jwk), redirectUri(redirectUri), code(code), scope(scope), codeVerifier(codeVerifier) {}
+    OidcPostRequest GetTokenRequest() const;
+};
+
+
+#endif //DRWHATSNOT_TOKENURL_H
